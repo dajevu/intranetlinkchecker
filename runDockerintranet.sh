80ec7c3c4218fc17999d880b7c2669bb41aa402c
dajevu <jdavis@zazarie.com>
2016-01-13 10:12:34 -0700

    update to startup script

--- runDockerintranet.sh
+++ runDockerintranet.sh
@@ -1,4 +1,4 @@
 
-docker run -d -e VMHOST=192.168.99.100 --name intranetlinker -p 8010:8010 jeffdavisco/intranetlinkchecker:latest /local/git/ntranetlinkchecker/run.sh
+docker run -d -e VMHOST=192.168.99.100 --name intranetlinker -p 8010:8010 jeffdavisco/intranetlinkchecker:latest /local/git/intranetlinkchecker/run.sh
 
 # once running you can identify the IP addr by using: boot2docker ip. Then, use that IP to access port 8080
